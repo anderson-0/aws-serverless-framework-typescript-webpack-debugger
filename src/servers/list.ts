49a0c548d601d2257730a41cdfe96280c63e448a
Anderson Silva <60013836+anderson-0@users.noreply.github.com>
2023-05-02 20:04:33 -0400

    updates

--- src/servers/list.ts
+++ src/servers/list.ts
@@ -1,8 +1,14 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
+import { DynamoDB } from "aws-sdk";
 
 export async function handler(event: APIGatewayProxyEvent) {
-
-
+  const dynamoDB = new DynamoDB.DocumentClient();
+  const getParams = {
+    Key: { userId: '' },
+    TableName: ''
+  }
+  const data = await dynamoDB.get(getParams).promise()
+  console.log(data.Item)
   return {
     statusCode: 200,
     body: JSON.stringify([]),
